0f513212fa3a98cd15df37630c2b49cb79448321
Keith Bostic <keith.bostic@mongodb.com>
2022-02-07 07:43:54 -0800

    update

--- search/pages_15.js
+++ search/pages_15.js
@@ -6,5 +6,6 @@ var searchData=
   ['wiredtiger_20community_20and_20contact_20information_3',['WiredTiger community and contact information',['../community.html',1,'index']]],
   ['wiredtiger_20licensing_4',['WiredTiger licensing',['../license.html',1,'index']]],
   ['wiredtiger_20overview_20and_20features_5',['WiredTiger Overview and Features',['../overview.html',1,'index']]],
-  ['writing_20wiredtiger_20applications_6',['Writing WiredTiger applications',['../programming.html',1,'index']]]
+  ['wiredtiger_20testing_6',['WiredTiger testing',['../testing.html',1,'index']]],
+  ['writing_20wiredtiger_20applications_7',['Writing WiredTiger applications',['../programming.html',1,'index']]]
 ];
